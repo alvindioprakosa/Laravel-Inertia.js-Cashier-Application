77348cbf478bb7d0174fa342dc148e9cbb913594
alvindioprakosa <alvindioprakosa@gmail.com>
2025-03-15 11:08:15 +0700

    Update Format

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
-  <head>
+
+<head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <link href="{{ asset('/admin/css/style.css') }}" rel="stylesheet" />
@@ -12,10 +13,12 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
     <script src="{{ asset('/admin/js/coreui.bundle.min.js') }}" defer></script>
     <script src="{{ mix('/js/app.js') }}" defer></script>
-  </head>
-  <body>
-      @inertia
+</head>
 
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-  </body>
+<body>
+    @inertia
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
 </html>
