f29ee4b14fd5396a64f01bd6614dff6011469952
alvindioprakosa <alvindioprakosa@gmail.com>
2025-04-01 23:42:43 +0700

    Update Sales

--- resources/views/exports/sales.blade.php
+++ resources/views/exports/sales.blade.php
@@ -1,37 +1,34 @@
-<div class="title" style="padding-bottom: 13px">
-    <div style="text-align: center;text-transform: uppercase;font-size: 15px">
+<div class="title pb-3 text-center">
+    <div class="fw-bold text-uppercase fs-6">
         Alvin Dio Prakosa
     </div>
-    <div style="text-align: center">
-        Alamat: Desa Kedungombo, Kec. Tengaran, Kab. Semarang
-    </div>
-    <div style="text-align: center">
-        Telp: 0857-9087-9087
-    </div>
+    <div>Alamat: Desa Kedungombo, Kec. Tengaran, Kab. Semarang</div>
+    <div>Telp: 0857-9087-9087</div>
 </div>
-<table style="width: 100%">
-    <thead>
-        <tr style="background-color: #e6e6e7;">
+
+<table class="table table-bordered w-100">
+    <thead class="bg-light">
+        <tr>
             <th scope="col">Date</th>
             <th scope="col">Invoice</th>
             <th scope="col">Cashier</th>
             <th scope="col">Customer</th>
-            <th scope="col">Total</th>
+            <th scope="col" class="text-end">Total</th>
         </tr>
     </thead>
     <tbody>
         @foreach($sales as $sale)
         <tr>
-            <td>{{ $sale->created_at }}</td>
+            <td>{{ \Carbon\Carbon::parse($sale->created_at)->format('d M Y') }}</td>
             <td>{{ $sale->invoice }}</td>
-            <td>{{ $sale->cashier->name ?? '' }}</td>
+            <td>{{ $sale->cashier->name ?? '-' }}</td>
             <td>{{ $sale->customer->name ?? 'Umum' }}</td>
-            <td class="text-end">{{ formatPrice($sale->grand_total) }}</td>
+            <td class="text-end">{{ number_format($sale->grand_total, 0, ',', '.') }}</td>
         </tr>
         @endforeach
-        <tr>
-            <td colspan="4" class="text-end fw-bold" style="background-color: #e6e6e7;">TOTAL</td>
-            <td class="text-end fw-bold" style="background-color: #e6e6e7;">{{ formatPrice($total) }}</td>
+        <tr class="fw-bold bg-light">
+            <td colspan="4" class="text-end">TOTAL</td>
+            <td class="text-end">{{ number_format($total, 0, ',', '.') }}</td>
         </tr>
     </tbody>
 </table>
